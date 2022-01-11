7b8e93584a56e0e622a0fad3a8acf98e25a81018
titacahyaa <95679672+titacahyaa@users.noreply.github.com>
2022-01-11 19:26:16 +0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{
 public static void main(String[] args){
 System.out.println("Tita adalah namaku");
-System.out.println("kuliahnya di ccit")
+System.out.println("kuliahnya di ccit");
 } }
